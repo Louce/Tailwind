259ef1ff35745d81a87e679e05e60bd212828978
Louce <79318424+Louce@users.noreply.github.com>
2023-09-24 23:40:56 +0200

    customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: { // même breakpoints que TW
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      colors: {
+        myPurple: {
+          100: "#8C4EEB",
+          200: "#5B0CD5",
+        },
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
