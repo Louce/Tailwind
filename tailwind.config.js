5d1a1dbfa6961ff914ae6c4e18e231f542144bf1
Louce <79318424+Louce@users.noreply.github.com>
2023-09-28 00:57:57 +0200

    tests:card, form, component TW, grid, flex, layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  content: ["./src/tutos/**/*.{html,js}"],
   theme: {
-    screens: { // même breakpoints que TW
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
+    container: {
+      center: true,
+    },
+    screens: {
+      // même breakpoints que TW
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     extend: {
+      fontSize: {
+        base: '18px',
+      },
       colors: {
         myPurple: {
           100: "#8C4EEB",
           200: "#5B0CD5",
         },
+      },
+      screens: {
+        xs: '350px',
+        xxs: '250px',
       },
     },
     plugins: [],
