53492f173def3069c72c14aacfccf1fec751e046
Louce <79318424+Louce@users.noreply.github.com>
2023-09-28 22:44:22 +0200

    exercice: toggle_button avec tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./src/components/**/*.{html,js}"],
   content: ["./src/**/*.{html,js}"],
-  content: ["./src/tutos/**/*.{html,js}"],
   theme: {
     container: {
       center: true,
@@ -15,18 +15,22 @@ module.exports = {
       "2xl": "1536px",
     },
     extend: {
-      fontSize: {
-        base: '18px',
-      },
       colors: {
         myPurple: {
           100: "#8C4EEB",
           200: "#5B0CD5",
         },
+        beige: "#ffedcd",
+        actif: "#8a63d2",
+        inactif: "#c5c0b8",
       },
       screens: {
         xs: '350px',
         xxs: '250px',
+      },
+      spacing: {
+        '5px': '5px',
+        30: '30px',
       },
     },
     plugins: [],
